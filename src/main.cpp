d7b13353283cde9111209cc13ebca96807b13048
yannlandry <yann.landry.94@gmail.com>
2014-12-30 16:24:47 -0400

    Enable image input from console

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,32 @@
 #include "edges.hpp"
 #include "sharpen.hpp"
 
-int main() {
-	cv::Mat image(cv::imread("swiss.jpg"));
-	cv::Mat edges;
+int main(int argc, char* argv[]) {
+	if(argc < 2) {
+		std::cerr << "Please enter the name of the image file in which to find edges." << std::endl;
+		return 0;
+	}
 
-	//std::cout << image.channels() << std::endl;
+	std::string imageFile(argv[1]);
+	cv::Mat image(cv::imread(imageFile));
+	
+	if(image.rows == 0 || image.cols == 0) {
+		std::cerr << "Please enter a valid image file." << std::endl;
+		return 0;
+	}
+
+	cv::Mat edges; // blank image
 	findEdges(image, edges);
 
-	cv::imshow("nyc.jpg", image);
-	cv::imshow("Edges of nyc.jpg", edges);
+	// show before & after
+	cv::imshow(imageFile, image);
+	cv::imshow("Edges of " + imageFile, edges);
+	
+	// convert & save image to file
+	edges.convertTo(edges, CV_8UC1, 255.0); 
+	cv::imwrite("edges." + imageFile, edges);
+
+	// wait
 	cv::waitKey(0);
 
 	return 0;
